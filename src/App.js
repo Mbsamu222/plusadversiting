5bb36383864e85a8d316050b85c276abacc6a556
Mbsamu222 <mbsamu22@gmail.com>
2024-04-27 17:10:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dropdown from './Components/Dropdown/Dropdown';
+import Home from './Pages/Home';
+import HomeCategory from './Pages/HomeCategory';
+import LoginSignup from './Pages/LoginSignup'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+          <Route>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<HomeCategory category='SERVICES' />} />
+          <Route path='/contact us' element={<HomeCategory category='CONTACTUS' />} />
+          <Route path='./loginsignup' element={<LoginSignup />} />
+          </Route>
+          
+          <Router>
+            
+                <Route exact path="/" component={Dropdown} />
+                <Route path="/compose" component={ComposePage} />
+          <Route path="/services" render={() => <HomeCategory category="SERVICES" />} />
+          <Route path="/contactus" render={() => <HomeCategory category="CONTACTUS" />} />
+          <Route path="/loginsignup" element={<LoginSignup/>}/>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+        
+
+
+        </Router>
     </div>
+
+    </div>
+
   );
 }
 
